67d3ff5134acb3555bb0a56dd2e904a31e641b62
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-04 08:16:45 -0700

    Agregar archivos de proyecto.

---
+++ PracticaPerimetro/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaPerimetro
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            controlPerimetroCirculo.Visibility = Visibility.Collapsed;
+            controlPerimetroCuadrado.Visibility = Visibility.Collapsed;
+
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Circulo
+                    controlPerimetroCirculo.Visibility = Visibility.Visible;
+                    break;
+                case 1: //Cuadrado
+                    controlPerimetroCuadrado.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            float perimetro = 0.0f;
+
+            switch(cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Circulo
+                    float radio = float.Parse(controlPerimetroCirculo.txtRadio.Text);
+                    perimetro = 2 * radio * 3.14159f;
+
+                    break;
+                case 1: //Cuadrado
+                    float lado = float.Parse(controlPerimetroCuadrado.txtLado.Text);
+                    perimetro = lado * 4;
+                    break;
+                default:
+                    break;
+            }
+            lblResultado.Text = perimetro.ToString();
+        }
+    }
+}
